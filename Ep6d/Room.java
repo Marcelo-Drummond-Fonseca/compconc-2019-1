84b92040c959d9cf2a504aed80fb840283cacf55
Marcelo-Drummond-Fonseca <48736981+Marcelo-Drummond-Fonseca@users.noreply.github.com>
2019-07-19 01:23:42 -0300

    Add files via upload

---
+++ Ep6d/Room.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+
+public class Room {
+    public static final int
+            TRIG_ON    = 1,
+            TRIG_OFF   = 2;
+            
+    Random random = new Random();
+
+    int trigger = random.nextInt(2) + 1;
+    boolean hasPrisoner = false;
+
+    public void setTrigger() {
+        this.trigger = TRIG_ON;
+    }
+
+    public void clearTrigger() {
+        this.trigger = TRIG_OFF;
+    }
+
+    public boolean isTriggerSet() {
+        return this.trigger == TRIG_ON;
+    }
+
+    public void closeDoor() {
+        System.out.println("Room: door closed.");
+        this.hasPrisoner = false;
+    }
+
+    public void openDoor() {
+        System.out.println("Room: door opened.");
+        this.hasPrisoner = true;
+    }
+
+    public boolean isDoorOpen() {
+        return this.hasPrisoner;
+    }
+}
